34d29c0f07be42269c2c13ab2e1a5b8b6b3d047d
DecentM <decentm@decentm.com>
2021-11-22 23:47:02 +0200

    feat: implement AudioLink haptics prefab
    Former-commit-id: 6f72c9f641f3a4f3a8b38e1bcd8aec75ddd37b90
    Former-commit-id: eed42a88d3861d1a65a0dc14aa87a4fa24f9926c
    Former-commit-id: 23b40dcf6f6c6e19cf286dfb989f3a564200ec62

--- prefabs-source/Assets/DecentM/Scripts/FollowPlayersVolume/FollowPlayersVolume.cs
+++ prefabs-source/Assets/DecentM/Scripts/FollowPlayersVolume/FollowPlayersVolume.cs
@@ -18,20 +18,19 @@ public class FollowPlayersVolume : UdonSharpBehaviour
     public int trackLimit = 5;
     [Tooltip("If false, it will only track the local player, regardless of the track limit setting")]
     public bool global = true;
+    [Tooltip("The link object will be spawned under the player's feet, and then offset by this vector.")]
+    public Vector3 linkOffset = new Vector3(0, 0, 0);
     
     private VRCPlayerApi[] followingPlayers;
+    private GameObject[] followingLinks;
 
-    private void Start()
+    private GameObject SpawnLinkClone ()
     {
-        // Initialise the array of currently followed players to the limit we're configured with
-        this.followingPlayers = new VRCPlayerApi[this.trackLimit];
+        GameObject playerLink = VRCInstantiate(this.link);
+        playerLink.SetActive(true);
+        playerLink.transform.SetPositionAndRotation(this.link.transform.position, this.link.transform.rotation);
 
-        // The link object is a template that we'll be instantiating in code later, so we disable the template
-        // so users don't see it.
-        if (this.link.activeSelf)
-        {
-            this.link.SetActive(false);
-        }
+        return playerLink;
     }
 
     private bool IsFollowingPlayer (VRCPlayerApi player)
@@ -53,14 +52,19 @@ public class FollowPlayersVolume : UdonSharpBehaviour
     private void FollowPlayer (VRCPlayerApi player)
     {
         Debug.Log($"Following {player.displayName}");
-        
+
         // Insert the player into the first empty index
+        // This has the side effect of not pushing players when the array length has reached
+        // the limit from this.trackLimit, since that's used for the length of the array
         for (int i = 0; i < this.followingPlayers.Length; i++)
         {
             if (this.followingPlayers[i] == null)
             {
                 this.followingPlayers[i] = player;
                 Debug.Log($"Pushed {player.displayName} on index {i}");
+
+                GameObject playerLink = this.SpawnLinkClone();
+                this.followingLinks[i] = playerLink;
                 break;
             }
         }
@@ -70,16 +74,61 @@ public class FollowPlayersVolume : UdonSharpBehaviour
     {
         Debug.Log($"Unfollowing {player.displayName}");
 
+        int i;
+
         // Remove the player from the array by setting its index to null
-        for (int i = 0; i < this.followingPlayers.Length; i++)
+        for (i = 0; i < this.followingPlayers.Length; i++)
         {
             if (this.followingPlayers[i] == player)
             {
                 this.followingPlayers[i] = null;
                 Debug.Log($"Removed {player.displayName} from index {i}");
+
+                Destroy(this.followingLinks[i]);
+                this.followingLinks[i] = null;
                 // We don't break here, to remove all occurrences of the player in case we have a bug
                 // where a player is duplicated within the array
             }
+        }
+    }
+
+    private void Start()
+    {
+        // Initialise the array of currently followed players to the limit we're configured with
+        this.followingPlayers = new VRCPlayerApi[this.trackLimit];
+        this.followingLinks = new GameObject[this.trackLimit];
+
+        // The link object is a template that we'll be instantiating in code later, so we disable the template
+        // so users don't see it.
+        if (this.link.activeSelf)
+        {
+            this.link.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < this.followingPlayers.Length; i++)
+        {
+            // This shouldn't happen, but just in case the consistency between links and players is broken,
+            // we bail from updating the broken link here. Players can fix this by leaving and re-entering the trigger.
+            if (this.followingLinks[i] == null)
+            {
+                return;
+            }
+
+            GameObject link = this.followingLinks[i];
+            VRCPlayerApi player = this.followingPlayers[i];
+
+            // If the player leaves the world while being tracked, we untrack them here
+            if (!player.IsValid())
+            {
+                this.UnfollowPlayer(player);
+                return;
+            }
+
+            // Move the link to the player
+            link.transform.SetPositionAndRotation(player.GetPosition() + this.linkOffset, player.GetRotation());
         }
     }
 
