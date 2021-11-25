6a0a318848cf65b27e4ffd638e382940baf18e68
DecentM <decentm@decentm.com>
2021-11-26 00:15:31 +0200

    refactor: finalise global sync behaviour on CustomToggle
    Former-commit-id: 6e45cd8f6bef77254466730815684ce00d91df2c
    Former-commit-id: ada6688a42c38174fa216d2041bdac18aa2f6192
    Former-commit-id: d2f53c881e7974328937098eba61921cb17d06e2

--- prefabs-source/Assets/DecentM/Scripts/BooleanAnimatorController/BooleanAnimatorController.cs
+++ prefabs-source/Assets/DecentM/Scripts/BooleanAnimatorController/BooleanAnimatorController.cs
@@ -2,7 +2,6 @@
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
-using DecentM;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class BooleanAnimatorController : UdonSharpBehaviour
@@ -14,75 +13,6 @@ public class BooleanAnimatorController : UdonSharpBehaviour
     public int layerIndex = 0;
     [Tooltip("The name of the parameter to toggle")]
     public string parameterName = "";
-
-    [Space]
-    [Tooltip("The default state of the Animators. If checked they will start playing when this UdonBehaviour starts.")]
-    public bool defaultState = false;
-    [Tooltip("If true, the state will be synced to everyone")]
-    public bool global = false;
-
-    [Header("LibDecentM")]
-    [Tooltip("The LibDecentM object")]
-    public LibDecentM lib;
-    [Tooltip("If checked, the list will function as a whitelist, otherwise it will function as a blacklist")]
-    public bool isWhitelist = false;
-    [Tooltip("If checked, only the instance master can use this trigger, and the player list will be ignored")]
-    public bool masterOnly = false;
-    [Tooltip("A list of players who can (or cannot) use this trigger")]
-    public PlayerList playerList;
-
-    void Start()
-    {
-        // Reset all targets to the default state
-        this.SetActive(this.defaultState);
-    }
-
-    public override void Interact()
-    {
-        VRCPlayerApi player = Networking.LocalPlayer;
-        bool isAllowed = this.lib.permissions.IsPlayerAllowed(player, this.masterOnly, this.isWhitelist, this.playerList);
-
-        if (!isAllowed)
-        {
-            return;
-        }
-
-        if (this.global)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.Toggle));
-        }
-        else
-        {
-            this.Toggle();
-        }
-    }
-
-    // Handle late joiners by firing ToggleOn/ToggleOff from the master
-    public override void OnPlayerJoined(VRCPlayerApi player)
-    {
-        // This isn't a permission check, only one player (the master) must be running this function
-        if (!Networking.LocalPlayer.isMaster || !this.global)
-        {
-            return;
-        }
-
-        bool isActive = this.animator.GetBool(this.parameterName);
-
-        // Skip sending a network update if the current state is the default one, to save on network resources
-        if (isActive == this.defaultState)
-        {
-            return;
-        }
-
-        if (isActive)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOn));
-        }
-        else
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOff));
-        }
-    }
 
     public void Toggle()
     {
---
+++ prefabs-source/Assets/DecentM/Scripts/CustomToggle/CustomToggle.cs
@@ -0,0 +1,91 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class CustomToggle : UdonSharpBehaviour
+{
+    [Tooltip("The default state of the toggle")]
+    public bool defaultState = false;
+    [Tooltip("If true, the state will be synced to everyone")]
+    public bool global = false;
+    [Space]
+    [Tooltip("The UdonBehaviour to send events to")]
+    public UdonBehaviour behaviour;
+    [Tooltip("The name of the event we send when toggling off")]
+    public string offEvent = "ToggleOff";
+    [Tooltip("The name of the event we send when toggling on")]
+    public string onEvent = "ToggleOn";
+
+    private bool state;
+
+    void Start()
+    {
+        // Reset to the default state
+        this.state = this.defaultState;
+    }
+
+    // Handle late joiners by firing ToggleOn/ToggleOff from the master
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        // This isn't a permission check, only one player (the master) must be running this function
+        if (!Networking.LocalPlayer.isMaster || !this.global)
+        {
+            return;
+        }
+
+        // Skip sending a network update if the current state is the default one, to save on network resources
+        if (this.state == this.defaultState)
+        {
+            return;
+        }
+
+        if (this.state == true)
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOn));
+        }
+        else
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOff));
+        }
+    }
+
+    public void Toggle()
+    {
+        // Invert the state
+        if (this.state == true)
+        {
+            this.ToggleOff();
+        }
+        else
+        {
+            this.ToggleOn();
+        }
+
+        if (this.global)
+        {
+            this.SyncState();
+        }
+    }
+
+    public void ToggleOn()
+    {
+        this.state = true;
+
+        this.behaviour.SendCustomEvent(this.onEvent);
+    }
+
+    public void ToggleOff()
+    {
+        this.state = false;
+
+        this.behaviour.SendCustomEvent(this.offEvent);
+    }
+
+    private void SyncState()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, this.state ? nameof(this.ToggleOn) : nameof(this.ToggleOff));
+    }
+}
