faa348af330bdd7b36c1e642d6c9cf3e04287d59
DecentM <decentm@decentm.com>
2021-11-26 00:22:54 +0200

    refactor: make PlayAnimator prefab use the new architecture
    Former-commit-id: cd8fb7509a61d66bca96f3bf910caeb2275f7caa
    Former-commit-id: 7de042d23540196012c78ffbab7a7dea95b2f4fc
    Former-commit-id: 3dfe78e65b446357bca62bbd82708abe08fd767c

--- prefabs-source/Assets/DecentM/Scripts/PlayAnimator/PlayAnimator.cs
+++ prefabs-source/Assets/DecentM/Scripts/PlayAnimator/PlayAnimator.cs
@@ -1,9 +1,6 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
-using DecentM;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class PlayAnimator : UdonSharpBehaviour
@@ -13,41 +10,8 @@ public class PlayAnimator : UdonSharpBehaviour
     public Animator[] targets;
     [Tooltip("Which animation to target in the controller")]
     public int layerIndex = 0;
-    [Tooltip("The default state of the Animators. If checked they will start playing when this UdonBehaviour starts.")]
-    public bool defaultState = false;
-    [Tooltip("If true, the state will be synced to everyone")]
-    public bool global = false;
     [Tooltip("Float between 0 and 1 - We will offset the animation by this much to count for network latency\nDoes nothing when `global` is unchecked")]
     public float latencyOffset = 0.01f;
-    [Tooltip("If checked, the animation will be toggled on/off.\nIf unchecked, the animation will be played each time the button is pressed.")]
-    public bool isToggle = false;
-
-    [Header("LibDecentM")]
-    [Tooltip("The LibDecentM object")]
-    public LibDecentM lib;
-    [Tooltip("If checked, the list will function as a whitelist, otherwise it will function as a blacklist")]
-    public bool isWhitelist = false;
-    [Tooltip("If checked, only the instance master can use this trigger, and the player list will be ignored")]
-    public bool masterOnly = false;
-    [Tooltip("A list of players who can (or cannot) use this trigger")]
-    public PlayerList playerList;
-
-    private void Start()
-    {
-        for (int i = 0; i < this.targets.Length; i++)
-        {
-            Animator target = this.targets[i];
-
-            if (this.defaultState == true)
-            {
-                this.PlayTarget(target);
-            }
-            else
-            {
-                this.StopTarget(target);
-            }
-        }
-    }
 
     private void PlayTarget(Animator animator)
     {
@@ -59,50 +23,6 @@ public class PlayAnimator : UdonSharpBehaviour
     private void StopTarget(Animator animator)
     {
         animator.enabled = false;
-    }
-
-    public override void Interact()
-    {
-        VRCPlayerApi player = Networking.LocalPlayer;
-        bool isAllowed = this.lib.permissions.IsPlayerAllowed(player, this.masterOnly, this.isWhitelist, this.playerList);
-
-        if (!isAllowed || this.targets[0] == null)
-        {
-            return;
-        }
-
-        if (this.targets[0].enabled && this.isToggle)
-        {
-            this.BroadcastStop();
-        }
-        else
-        {
-            this.BroadcastPlay();
-        }
-    }
-
-    private void BroadcastPlay()
-    {
-        if (this.global)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.Play));
-        }
-        else
-        {
-            this.Play();
-        }
-    }
-
-    private void BroadcastStop()
-    {
-        if (this.global)
-        {
-            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.Stop));
-        }
-        else
-        {
-            this.Stop();
-        }
     }
 
     public void Play()
