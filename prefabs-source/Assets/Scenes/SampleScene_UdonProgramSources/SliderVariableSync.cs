35dd4da8698bb7444b096e0d473692a6be621492
DecentM <decentm@decentm.com>
2021-12-01 23:34:31 +0200

    feat: add settings UI for ManualCamera and AudioLinkHapticsVolume
    Former-commit-id: b6c211f3393cdb654fb42d27fc5ffc8e59a91af2
    Former-commit-id: 3ae7be397d2fee8beb492b6b2d35c5ac45ab3e80
    Former-commit-id: 1b31c4ada6ca3d98c8ab2c72f8403f2e2d6798dc

---
+++ prefabs-source/Assets/Scenes/SampleScene_UdonProgramSources/SliderVariableSync.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using TMPro;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class SliderVariableSync : UdonSharpBehaviour
+{
+    public UdonBehaviour behaviour;
+    public TextMeshProUGUI textMesh;
+    public string variableName;
+
+    private Slider slider;
+
+    private void Start()
+    {
+        this.slider = GetComponent<Slider>();
+
+        this.OnUpdate();
+    }
+
+    public void OnUpdate()
+    {
+        float value = this.slider.value;
+        this.behaviour.SetProgramVariable(this.variableName, value);
+        this.textMesh.text = $"{this.variableName}: {value.ToString("0.00")}";
+    }
+}
