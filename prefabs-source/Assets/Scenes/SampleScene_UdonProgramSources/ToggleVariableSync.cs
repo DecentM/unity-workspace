d4ade9a9282653a249ce1057f576441d9b7b835e
DecentM <decentm@decentm.com>
2021-12-02 18:24:17 +0200

    feat: add isMaster control, fix toggle variable sync
    Former-commit-id: 7ca7643568befd9727c8b5f4fb1e604b0287c35a
    Former-commit-id: bbe9f98b1b85fba9813234c4b1e4082587ae3493
    Former-commit-id: 1a80538daa9b243c9e18c6b2cca628c4f15525b1

---
+++ prefabs-source/Assets/Scenes/SampleScene_UdonProgramSources/ToggleVariableSync.cs
@@ -0,0 +1,85 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+using TMPro;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class ToggleVariableSync : UdonSharpBehaviour
+{
+    public UdonBehaviour behaviour;
+    public Text textMesh;
+    public string variableName;
+
+    private Toggle toggle;
+
+    private void Start()
+    {
+        this.toggle = GetComponent<Toggle>();
+        this.textMesh.text = this.variableName;
+    }
+
+    public void OnUpdate()
+    {
+        if (this.toggle.isOn)
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOn));
+        } else
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOff));
+        }
+
+        this.DoToggle(this.toggle.isOn);
+    }
+
+    private void DoToggle(bool value)
+    {
+        this.behaviour.SetProgramVariable(this.variableName, value);
+        this.textMesh.text = this.variableName;
+    }
+
+    public void ToggleOn()
+    {
+        // Prevent infinite loops by doing a sanity check here
+        if (this.toggle.isOn)
+        {
+            return;
+        }
+
+        this.DoToggle(true);
+        this.toggle.SetIsOnWithoutNotify(true);
+    }
+
+    public void ToggleOff()
+    {
+        // Prevent infinite loops by doing a sanity check here
+        if (!this.toggle.isOn)
+        {
+            return;
+        }
+
+        this.DoToggle(false);
+        this.toggle.SetIsOnWithoutNotify(false);
+    }
+
+    // Handle late joiners by firing ToggleOn/ToggleOff from the master
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        // This isn't a permission check, only one player (the master) must be running this function
+        if (!Networking.LocalPlayer.isMaster)
+        {
+            return;
+        }
+
+        if (this.toggle.isOn)
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOn));
+        }
+        else
+        {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(this.ToggleOff));
+        }
+    }
+}
