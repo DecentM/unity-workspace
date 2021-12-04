f704b36adeb7682355c4e2b7bb272d272b9391d3
DecentM <decentm@decentm.com>
2021-12-04 08:06:38 +0200

    chore: create files for AvatarEnforcer
    Former-commit-id: dc4531933fc6885581c8f25473bdc9cafbb495d7
    Former-commit-id: f6c62a9c8d9eb47729701d61eaa0ca2e9736fa0d
    Former-commit-id: 0dda778dc6a79084a4d9b7fea3e467fd85199851

---
+++ prefabs-source/Assets/DecentM/Scripts/AvatarEnforcer/AvatarEnforcer.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using UnityEngine.UI;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class AvatarEnforcer : UdonSharpBehaviour
+{
+    public float wantedJawPos = 1.2121266f;
+    public Text debugText;
+
+    void Start()
+    {
+        Vector3 jawPos = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Hips);
+
+        bool flag = jawPos.y == this.wantedJawPos;
+
+        if (flag == true)
+        {
+            this.debugText.text = $"Avatar accepted, bone position was {jawPos}, expected {this.wantedJawPos}";
+        } else {
+            this.debugText.text = $"Avatar denied, bone position was {jawPos}, expected {this.wantedJawPos}";
+        }
+
+        if (jawPos.y == 0)
+        {
+            Debug.Log("We're probably in the editor");
+        }
+    }
+}
