d6846e58df044924b5d1d8e3006974fd71309621
DecentM <decentm@decentm.com>
2021-12-07 02:51:09 +0200

    refactor: use a the normalised position for bones in AvatarEnforcer
    Former-commit-id: 11a433e3354c59d99cde1c283e9a334118edf76b
    Former-commit-id: 6508f180b6b8ddb26e7ecbbb76f22275e38df08d
    Former-commit-id: 32ffd106e37a8774dd12bc93fb7ead715f38aefc

--- prefabs-source/Assets/DecentM/Scripts/AvatarEnforcer/AvatarEnforcer.cs
+++ prefabs-source/Assets/DecentM/Scripts/AvatarEnforcer/AvatarEnforcer.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 using VRC.SDKBase;
 using UnityEngine.UI;
 using VRC.Udon;
+using DecentM;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class AvatarEnforcer : UdonSharpBehaviour
 {
-    public float wantedJawPos = 1.2121266f;
+    [Header("LibDecentM")]
+    [Tooltip("The LibDecentM object")]
+    public LibDecentM lib;
+
+    // public float wantedJawPos = 1.2121266f;
     public Text debugText;
+    // public HumanBodyBones targetBone1 = HumanBodyBones.Hips;
+    public HumanBodyBones targetBone = HumanBodyBones.Head;
+    // public HumanBodyBones targetBoneRoot = HumanBodyBones.Neck;
 
     void Start()
     {
-        Vector3 jawPos = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Hips);
+        this.debugText.text = "Starting...";
 
-        bool flag = jawPos.y == this.wantedJawPos;
+        this.lib.scheduling.OnEverySecond((UdonBehaviour) GetComponent(typeof(UdonBehaviour)));
+    }
 
-        if (flag == true)
-        {
-            this.debugText.text = $"Avatar accepted, bone position was {jawPos}, expected {this.wantedJawPos}";
-        } else {
-            this.debugText.text = $"Avatar denied, bone position was {jawPos}, expected {this.wantedJawPos}";
-        }
+    public void OnSecondPassed()
+    {
+        // Quaternion boneRot1 = Networking.LocalPlayer.GetBoneRotation(this.targetBone1);
+        // Quaternion boneRot2 = Networking.LocalPlayer.GetBoneRotation(this.targetBone2);
 
-        if (jawPos.y == 0)
-        {
-            Debug.Log("We're probably in the editor");
-        }
+        // Vector3 bonePos1 = Networking.LocalPlayer.GetBonePosition(this.targetBone1);
+        // Vector3 bonePos = Networking.LocalPlayer.GetBonePosition(this.targetBone);
+        // Vector3 rootPos = Networking.LocalPlayer.GetBonePosition(this.targetBoneRoot);
+
+        VRCPlayerApi.TrackingData td = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+
+        // if (bonePos1.y == 0)
+        // {
+        // this.debugText.text = $"{this.targetBone1} was not found";
+        // return;
+        // }
+
+        // if (bonePos2.y == 0)
+        // {
+        // this.debugText.text = $"{this.targetBone2} was not found";
+        // return;
+        // }
+
+        // float diff = Mathf.Round((bonePos2.y - bonePos1.y) * 10000);
+        // float rotdiff = Mathf.Round((boneRot1.w - boneRot2.w) * 1000);
+
+        this.debugText.text = $"{this.targetBone} {td.position.normalized}";
     }
 }
