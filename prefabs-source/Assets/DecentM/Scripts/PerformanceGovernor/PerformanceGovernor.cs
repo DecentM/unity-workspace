45ba4127b61f4b297259b20a4f46f24e6dd86158
DecentM <decentm@decentm.com>
2021-12-17 19:23:31 +0200

    feat: create initial version of PerformanceGovernor
    Former-commit-id: bd5ba1372c83bf5a4ce4b1b93c52b32e2e9821e0
    Former-commit-id: 8cb41eca5df2b19e5cb38994bd4c1860e357b194
    Former-commit-id: 0a7eb5d84815b2898659d8c8733350e9b36384cf

---
+++ prefabs-source/Assets/DecentM/Scripts/PerformanceGovernor/PerformanceGovernor.cs
@@ -0,0 +1,72 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class PerformanceGovernor : UdonSharpBehaviour
+{
+    private const int CheckEverySeconds = 10;
+
+    private float updateRate;
+    private int fixedClock = 0;
+    private int frameCounter = 0;
+    private int fpsAverage = 0;
+
+    [HideInInspector]
+    public int high = 60;
+    [HideInInspector]
+    public int low = 30;
+    public TextMeshProUGUI debug;
+
+    private void Start()
+    {
+        this.updateRate = 1 / Time.fixedDeltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        this.fixedClock++;
+    
+        // If we've reached one second, the current frameCounter will be our FPS value
+        if (this.fixedClock % this.updateRate == 0)
+        {
+            // int secondsSinceLastCheck = Mathf.RoundToInt(this.fixedClock / this.updateRate);
+            this.fpsAverage = (this.frameCounter + this.fpsAverage) / 2;
+            this.frameCounter = 0;
+        }
+
+        // Every ten seconds, check frames and send events as needed
+        if (this.fixedClock % (this.updateRate * CheckEverySeconds) == 0)
+        {
+            this.CheckFrames();
+            this.fixedClock = 0;
+            this.fpsAverage = 0;
+        }
+
+        if (this.debug != null && this.fixedClock % (this.updateRate / 10) == 0)
+        {
+            this.PrintDebugText();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        this.frameCounter++;
+    }
+
+    private void PrintDebugText()
+    {
+        this.debug.text = "";
+        this.debug.text += $"Average FPS: {this.fpsAverage}\n";
+        this.debug.text += $"Frame counter: {this.frameCounter}\n";
+    }
+
+    private void CheckFrames()
+    {
+        // Check the current framerate and increase of decrease the level based on the current FPS + some headroom
+        // to prevent switching back and forth quickly
+    }
+}
---
+++ prefabs-source/Assets/DecentM/Scripts/PerformanceGovernor/PerformanceGovernorInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PerformanceGovernor))]
+public class PerformanceGovernorInspector : Editor
+{
+    private const int AbsoluteHigh = 90;
+    private const int AbsoluteLow = 20;
+    private const int VerticalSpacing = 30;
+
+    // private int high = 60;
+    // private float med = 40;
+    // private int low = 30;
+
+    public override void OnInspectorGUI()
+    {
+        PerformanceGovernor governor = Selection.activeGameObject.GetComponent<PerformanceGovernor>();
+
+        Rect hArea = EditorGUILayout.BeginVertical(GUILayout.Height(VerticalSpacing));
+        float newHigh = EditorGUILayout.Slider(new GUIContent("High", "Current performance will be considered high above this value"), governor.high, governor.low + 1, AbsoluteHigh);
+        governor.high = Mathf.RoundToInt(newHigh);
+        EditorGUILayout.EndVertical();
+
+        Rect lArea = EditorGUILayout.BeginVertical(GUILayout.Height(VerticalSpacing));
+        float newLow = EditorGUILayout.Slider(new GUIContent("Low", "Current performance will be considered low below this value"), governor.low, AbsoluteLow, governor.high - 1);
+        governor.low = Mathf.RoundToInt(newLow);
+        EditorGUILayout.EndVertical();
+
+        DrawDefaultInspector();
+    }
+}
