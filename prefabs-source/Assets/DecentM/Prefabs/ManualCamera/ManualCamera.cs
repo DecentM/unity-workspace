075c98569cf26dc3792364e9c26366dc5f2334ea
DecentM <decentm@decentm.com>
2022-04-23 16:05:45 +0300

    chore: reorganise prefab assets to be in one folder per prefab, remove unused prefabs and scripts
    Former-commit-id: 64c1ce3427d756a0a5b7f434f283fa305a8ea42c
    Former-commit-id: 9a19219121939cf146a53493ee3ee90d459f5937
    Former-commit-id: fb75fc1fe9ee9bad9e5f05e877a4deb4b869f986

---
+++ prefabs-source/Assets/DecentM/Prefabs/ManualCamera/ManualCamera.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class ManualCamera : UdonSharpBehaviour
+{
+    private new Camera camera;
+
+    private void Start()
+    {
+        this.camera = GetComponent<Camera>();
+        this.camera.enabled = false;
+    }
+
+    public void Render()
+    {
+        this.camera.Render();
+    }
+
+    public int targetFps = 30;
+    private float elapsed;
+
+    private void LateUpdate()
+    {
+        this.elapsed += Time.unscaledDeltaTime;
+        if (this.elapsed > 1 / this.targetFps)
+        {
+            this.elapsed = 0;
+            this.camera.Render();
+        }
+    }
+}
