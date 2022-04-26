2931387ed7dde0ea9cbf70b49369fd766536c8f6
DecentM <decentm@decentm.com>
2022-04-27 00:56:42 +0300

    feat: initialise metrics-server
    Former-commit-id: ca85a21c08036cc4ee993b9058c81c8d72de72dc
    Former-commit-id: 4b5ea56ef48a3ed5f41524db8bca9be24a7369a0
    Former-commit-id: 8e0b35dc4bf28c2c924e4cb59a68243d38f52214

---
+++ prefabs-source/MetricsServer/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  quoteProps: "consistent",
+  bracketSpacing: false,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+}
