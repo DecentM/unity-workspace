3c185c207127e4bdbfb1699390f7327d2e5e186f
DecentM <decentm@decentm.com>
2022-05-28 20:02:44 +0300

    feat: create self-contained random instance class
    Former-commit-id: d101d7401937021e09308405dad5f3901a157ef9
    Former-commit-id: 2ecd86bd2cfe2cc8e604228ace759de31bf9ec67
    Former-commit-id: b55e78113a78154fa59cc01a87b668cfa1dcabbb

---
+++ prefabs-source/Assets/DecentM/Prefabs/Procedural/Scripts/ProceduralSystem.cs
@@ -0,0 +1,42 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+using DecentM.Tools;
+
+namespace DecentM.Procedural
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ProceduralSystem : UdonSharpBehaviour
+    {
+        public RandomInstance random;
+
+        [UdonSynced, FieldChangeCallback(nameof(seed))]
+        private int _seed;
+
+        public int seed
+        {
+            get => _seed;
+            set
+            {
+                this._seed = value;
+                this.HandleSeedChange();
+            }
+        }
+
+        private void Start()
+        {
+            if (Networking.GetOwner(this.gameObject) != Networking.LocalPlayer)
+                return;
+
+            this.seed = 6;
+            this.RequestSerialization();
+        }
+
+        private void HandleSeedChange()
+        {
+            this.random.SetSeed(this.seed);
+        }
+    }
+}
