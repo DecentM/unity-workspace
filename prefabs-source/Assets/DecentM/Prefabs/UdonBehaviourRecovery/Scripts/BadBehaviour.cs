f22a0ceddb85f05d6f61011b6a417420f4cd415c
DecentM <decentm@decentm.com>
2022-06-10 20:50:31 +0300

    refactor: rework UdonBehaviourRecovery so that it works by instantiating an inactive template
    Former-commit-id: e0da0002f813d02334638669b78aa4788b429655
    Former-commit-id: 83bd713a9f42c141be48c5e430efed18c17205ef
    Former-commit-id: daef964f51cec987e8b15d63be5c5ee450285365

---
+++ prefabs-source/Assets/DecentM/Prefabs/UdonBehaviourRecovery/Scripts/BadBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+namespace DecentM.UdonBehaviourRecovery
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
+    public class BadBehaviour : UdonSharpBehaviour
+    {
+        public TextMeshProUGUI tmp;
+
+        [UdonSynced, FieldChangeCallback(nameof(continuousSync))]
+        private float _continuousSync;
+
+        public float continuousSync
+        {
+            get { return this._continuousSync; }
+            set
+            {
+                this._continuousSync = value;
+                this.UpdateContSync();
+            }
+        }
+
+        public GameObject rotatedObject;
+
+        private void UpdateContSync()
+        {
+            this.tmp.text = $"{this.continuousSync}";
+        }
+
+        private void FixedUpdate()
+        {
+            if (Networking.GetOwner(this.gameObject) != Networking.LocalPlayer)
+                return;
+
+            this.continuousSync = Time.time;
+
+            this.rotatedObject.transform.rotation = Quaternion.Euler(
+                Mathf.Sin(this.continuousSync) * 90,
+                0,
+                0
+            );
+        }
+
+        private GameObject nonexist;
+
+        public override void Interact()
+        {
+            Debug.Log($"[BadBehaviour] Crashing on purpose...");
+            this.nonexist.SetActive(false);
+        }
+    }
+}
---
+++ prefabs-source/Assets/DecentM/Prefabs/UdonBehaviourRecovery/Scripts/UdonBehaviourRecovery.cs
@@ -0,0 +1,94 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.Udon;
+using VRC.SDKBase;
+using DecentM;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class UdonBehaviourRecovery : UdonSharpBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Check for crashed UdonBehaviours every N seconds")]
+    public int pollIntervalSeconds = 60;
+
+    public UdonBehaviour template;
+    private GameObject instance;
+
+    private void Start()
+    {
+        if (this.template.gameObject == this.gameObject)
+        {
+            Debug.LogError(
+                $"The monitored UdonBehaviour must be on a different game object from the UdonBehaviourRecovery script."
+            );
+            this.enabled = false;
+            return;
+        }
+
+        if (this.template.gameObject.activeSelf)
+        {
+            Debug.LogError($"The template containing the UdonBehaviour must be disabled.");
+            this.enabled = false;
+            return;
+        }
+
+        this.CreateInstance();
+    }
+
+    private float elapsed = 0;
+
+    private void FixedUpdate()
+    {
+        this.elapsed += Time.fixedUnscaledDeltaTime;
+        if (this.elapsed <= this.pollIntervalSeconds)
+            return;
+        this.elapsed = 0;
+
+        if (!this.Check())
+            this.Fix();
+    }
+
+    private bool Check()
+    {
+        if (this.instance == null)
+            return false;
+
+        UdonBehaviour behaviour = this.instance.GetComponent<UdonBehaviour>();
+
+        if (behaviour == null || !behaviour.gameObject.activeSelf || !behaviour.enabled)
+            return false;
+
+        return true;
+    }
+
+    private void DestroyInstance()
+    {
+        UdonBehaviour behaviour = this.instance.GetComponent<UdonBehaviour>();
+
+        if (behaviour != null)
+            behaviour.enabled = false;
+
+        this.instance.SetActive(false);
+        Destroy(this.instance.gameObject);
+        this.instance = null;
+    }
+
+    private void CreateInstance()
+    {
+        if (this.instance != null)
+            this.DestroyInstance();
+
+        this.instance = Instantiate(this.template.gameObject);
+        this.instance.name = $"{this.template.name}_instance";
+        this.instance.SetActive(true);
+        this.instance.transform.SetParent(this.gameObject.transform, true);
+    }
+
+    private void Fix()
+    {
+        if (this.instance != null)
+            this.DestroyInstance();
+
+        this.CreateInstance();
+    }
+}
