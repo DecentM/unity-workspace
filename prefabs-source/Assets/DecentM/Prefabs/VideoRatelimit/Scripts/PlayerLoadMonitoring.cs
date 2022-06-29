2c1d4034046919bebff222079b0ca28c2a2d888a
Barnabás Bertényi <barnabas+git@bertenyi.hu>
2022-06-29 13:18:53 +0200

    feat(VideoPlayer): make the base VRC video player a required component for video player monitoring
    Former-commit-id: 430abf41aeb44d4f09d893749c03f506e9243882
    Former-commit-id: e4bc71203537f692f15de93e21036df3c8bd3152
    Former-commit-id: 32fe021ce28cab33a486140d4d617fb180759ff0

--- prefabs-source/Assets/DecentM/Prefabs/VideoRatelimit/Scripts/PlayerLoadMonitoring.cs
+++ prefabs-source/Assets/DecentM/Prefabs/VideoRatelimit/Scripts/PlayerLoadMonitoring.cs
@@ -2,10 +2,11 @@
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.SDK3.Video.Components.Base;
 
 namespace DecentM.VideoRatelimit
 {
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None), RequireComponent(BaseVRCVideoPlayer)]
     public class PlayerLoadMonitoring : UdonSharpBehaviour
     {
         public VideoRatelimitSystem system;
