ff58295a0fcd839123150e3317377660c9d6097f
DecentM <decentm@decentm.com>
2022-07-30 05:00:30 +0300

    feat: initial setup

---
+++ mods/TestMod.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+
+namespace DecentM.Mods
+{
+    public class TestMod : MelonMod
+    {
+        public override void OnApplicationLateStart()
+        {
+            LoggerInstance.Msg("[DecentM] OnApplicationStart();");
+        }
+    }
+}
