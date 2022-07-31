9f2beca6d186ed2d79b1b24d5e365a1bd7784b1a
DecentM <decentm@decentm.com>
2022-07-31 03:16:42 +0300

    ci: merge steamcmd login and game setup steps because you need to log in for that anyway

--- mods/tools/bootstrap/game.ps1
+++ mods/tools/bootstrap/game.ps1
@@ -1,8 +1,20 @@
 $gameid = 661130
 $folder = "ChilloutVR"
 
+if ($null -eq $env:STEAM_USER) {
+    Write-Output "The STEAM_USER environment variable is not set. Cannot download the game."
+    exit 1
+}
+
+if ($null -eq $env:STEAM_PASSWORD) {
+    Write-Output "The STEAM_PASSWORD environment variable is not set. Cannot download the game."
+    exit 2
+}
+
 .\SteamCMD\steamcmd\steamcmd.exe `
     +@sSteamCmdForcePlatformType windows `
     +force_install_dir ../../$folder `
+    +login $env:STEAM_USER "$env:STEAM_PASSWORD" `
     +app_update $gameid validate `
-    +quit
+    +quit `
+    || & { "ignore failure"; $global:LASTEXITCODE = 0 }
