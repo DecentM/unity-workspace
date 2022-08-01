d1915237d99d2aa1f4130ad068d434458550ab5b
DecentM <decentm@decentm.com>
2022-08-01 23:12:57 +0300

    feat: create ComponentsUnleashed mod

---
+++ mods/Mods/ComponentsUnleashed/ComponentsUnleashed.cs
@@ -0,0 +1,14 @@
+﻿using MelonLoader;
+
+namespace DecentM.Mods.ComponentsUnleashed
+{
+    public class Mod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            LoggerInstance.Msg("Patching...");
+            Patcher.Patch();
+            LoggerInstance.Msg("After patching");
+        }
+    }
+}
---
+++ mods/Mods/ComponentsUnleashed/Patcher.cs
@@ -0,0 +1,12 @@
+﻿using DecentM.Mods.ComponentsUnleashed.Patches;
+
+namespace DecentM.Mods.ComponentsUnleashed
+{
+    internal class Patcher
+    {
+        public static void Patch()
+        {
+            HarmonyLib.Harmony.CreateAndPatchAll(typeof(ExtendComponentWhitelistPatch));
+        }
+    }
+}
