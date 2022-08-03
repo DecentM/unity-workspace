e6df491c2920ed1c88c85b6bf14cc3abe2c6b45d
DecentM <decentm@decentm.com>
2022-08-03 06:49:43 +0300

    chore: update ComponentsUnleashed to use built reference from github
    Former-commit-id: e06bf348a57b38fe279813652a84359fff4a55d7
    Former-commit-id: f77c1274b2c32ac6b9f04c320462cc59d4d2a705

--- mods/Mods/ComponentsUnleashed/ComponentsUnleashed.cs
+++ mods/Mods/ComponentsUnleashed/ComponentsUnleashed.cs
@@ -6,8 +6,17 @@ using HarmonyLib;
 
 using ABI_RC.Core;
 
+using DecentM.Prefabs;
+using DecentM.Prefabs.Pubsub;
+using DecentM.Prefabs.Performance;
+using DecentM.Prefabs.UI;
 using DecentM.Prefabs.VideoPlayer;
+using DecentM.Prefabs.VideoPlayer.Plugins;
 using DecentM.Prefabs.VideoPlayer.Handlers;
+using DecentM.Prefabs.Notifications;
+using DecentM.Prefabs.Notifications.Providers;
+using DecentM.Prefabs.PlayerList;
+using DecentM.Prefabs.VideoRatelimit;
 
 namespace DecentM.Mods.ComponentsUnleashed
 {
@@ -21,19 +30,19 @@ namespace DecentM.Mods.ComponentsUnleashed
             /*
              * PubSub
              */
-            whiteList.Add(typeof(Pubsub.PubsubHost));
-            whiteList.Add(typeof(Pubsub.PubsubSubscriber));
+            whiteList.Add(typeof(PubsubHost));
+            whiteList.Add(typeof(PubsubSubscriber));
 
             /*
              * PerformanceGovernor
              */
-            whiteList.Add(typeof(Prefabs.Performance.PerformanceGovernor));
+            whiteList.Add(typeof(PerformanceGovernor));
 
             /*
              * UI
              */
-            whiteList.Add(typeof(Prefabs.UI.Dropdown));
-            whiteList.Add(typeof(Prefabs.UI.DropdownOption));
+            whiteList.Add(typeof(Dropdown));
+            whiteList.Add(typeof(DropdownOption));
 
             /*
              * VideoPlayer
@@ -41,13 +50,64 @@ namespace DecentM.Mods.ComponentsUnleashed
             whiteList.Add(typeof(UnityEngine.Video.VideoPlayer));
             
             // Core
-            whiteList.Add(typeof(PlayerHandler));
+            whiteList.Add(typeof(UnityVideo));
             whiteList.Add(typeof(ScreenHandler));
             whiteList.Add(typeof(VideoPlayerEvents));
             whiteList.Add(typeof(VideoPlayerSystem));
             whiteList.Add(typeof(VideoPlayerUI));
 
             // Plugins
+            whiteList.Add(typeof(AutoLivestreamDetectorPlugin));
+            whiteList.Add(typeof(AutoPlayPlugin));
+            whiteList.Add(typeof(AutoRetryPlugin));
+            whiteList.Add(typeof(DebugPlugin));
+            whiteList.Add(typeof(LoadRequestHandlerPlugin));
+            whiteList.Add(typeof(RenderTextureExportPlugin));
+            whiteList.Add(typeof(ResolutionUpdaterPlugin));
+            whiteList.Add(typeof(ScreenAnalysisPlugin));
+            whiteList.Add(typeof(SkipToTimestampPlugin));
+            whiteList.Add(typeof(SoundEffectsPlugin));
+            whiteList.Add(typeof(SubtitlesPlugin));
+            whiteList.Add(typeof(TextureReferencePlugin));
+            whiteList.Add(typeof(TextureUpdaterPlugin));
+            whiteList.Add(typeof(UIPlugin));
+            whiteList.Add(typeof(URLVerifierPlugin));
+
+            // Playlist Plugins
+            whiteList.Add(typeof(PlaylistItemRenderer));
+            whiteList.Add(typeof(PlaylistPlayerPlugin));
+            whiteList.Add(typeof(VideoPlaylist));
+            whiteList.Add(typeof(VideoPlaylistUI));
+
+            /*
+             * VideoRatelimit
+             */
+            whiteList.Add(typeof(PlayerLoadMonitoring));
+            whiteList.Add(typeof(VideoRatelimitSystem));
+
+            /*
+             * NotificationSystem
+             */
+            whiteList.Add(typeof(NotificationSystem));
+            whiteList.Add(typeof(PerformanceLevelChangeProvider));
+
+            /*
+             * ManualCamera
+             */
+            whiteList.Add(typeof(ManualCamera));
+
+            /*
+             * LibDecentM
+             */
+            whiteList.Add(typeof(Debugging));
+            whiteList.Add(typeof(LibDecentM));
+            whiteList.Add(typeof(PlayerList));
+            whiteList.Add(typeof(Scheduling));
+
+            /*
+             * CustomEvent
+             */
+            whiteList.Add(typeof(TimedCustomEvent));
 
             Traverse.Create<CVRTools>().Field("componentWhiteList").SetValue(whiteList);
         }
