e8543d5f45e071af5d6fb5272af72c04caae526e
DecentM <decentm@decentm.com>
2022-08-03 07:23:59 +0300

    fix(ci): fix bootstrap clean script
    Former-commit-id: 3d8350bf9cbb55345231a544f7920dc065bc1ebb
    Former-commit-id: 842b26519bfc2a89541ce96995596ffc73f23763

--- mods/tools/clean.ps1
+++ mods/tools/clean.ps1
@@ -1,5 +1,10 @@
-$delfolders = ".\bin .\ChilloutVR .\MelonLoader .\SteamCMD .\NuGet .\7-Zip .\DecentM.Prefabs"
+$delfolders = ".\bin", ".\ChilloutVR", ".\MelonLoader", ".\SteamCMD", ".\NuGet", ".\7-Zip", ".\DecentM.Prefabs"
 
-Write-Output "Recursively deleting: $delfolders"
-Remove-Item -Force -Recurse $delfolders
+foreach ($delfolder in $delfolders) {
+    if (Test-Path -Path $delfolder) {
+        Write-Output "Recursively deleting: $delfolder"
+        Remove-Item -Force -Recurse $delfolder
+    }
+}
+
 Write-Output "Done. Run .\tools\bootstrap_all.ps1 to now do a clean installation!"
