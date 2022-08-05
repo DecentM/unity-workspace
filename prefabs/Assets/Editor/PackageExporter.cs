b4dbd3f71e19f87496cfdb733e3a30c2d822b9d6
DecentM <decentm@decentm.com>
2022-08-06 02:17:57 +0300

    ci: set up auto exports
    Former-commit-id: 388386d86555f29ec22b36549ba81fd70e5c3436
    Former-commit-id: 03e6c3998a29ce2c52108e437f75a051bba8011c

---
+++ prefabs/Assets/Editor/PackageExporter.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEditor;
+
+public static class PackageExporter
+{
+	private const string name = "DecentM.Prefabs";
+
+	private static string[] paths = new string[]
+	{
+		"Assets/DecentM",
+		"Assets/Packages",
+		"Assets/UserLibs"
+	};
+
+	[MenuItem("DecentM/PackageExporter/Export Package")]
+	public static void ExportPackage()
+	{
+		string output = $"./output/{name}.unitypackage";
+
+		DirectoryInfo dir = new FileInfo(output).Directory;
+
+		if (dir != null && !dir.Exists)
+			dir.Create();
+
+		AssetDatabase.ExportPackage(
+			paths,
+			output,
+			ExportPackageOptions.Recurse
+		);
+	}
+}
