dd14de752667ef0672bb194c0f52b4dde9edb3d5
DecentM <decentm@decentm.com>
2022-08-07 12:25:08 +0300

    ci: blindly move actions and scripts to the root folder
    Former-commit-id: 29a46e34115bd7b066d8ddbff5351ba59f3f82fe

--- prefabs/tools/vars.ps1
+++
@@ -1,8 +0,0 @@
-$env:PREFABS_REPO = "DecentM/world-prefabs-source"
-$env:PREFABS_FOLDER = "prefabs"
-# PREFABS_ARTIFACT_NAME - Set from CI as env variable
-# PREFABS_RUN_ID - Set from CI as env variable
-
-$env:PREFABS_VERSION = "v1.0.3"
-$env:PREFABS_FILENAME = "DecentM.Components.zip"
-$env:PREFABS_URL = "https://github.com/$env:PREFABS_REPO/releases/download/$env:PREFABS_VERSION/$env:PREFABS_FILENAME"
