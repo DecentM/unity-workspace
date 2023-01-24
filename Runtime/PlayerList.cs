f73cf3c380c485d1bd1f519ee82a70d938a104de
DecentM <decentm@decentm.com>
2023-01-24 23:59:18 +0200

    chore(video-player): v0.0.2

---
+++ Runtime/PlayerList.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+using DecentM.Collections;
+using DecentM.Pubsub;
+
+namespace DecentM.PlayerList
+{
+    public enum PlayerListEvent
+    {
+        PlayerAdded,
+        PlayerRemoved,
+    }
+
+    public class PlayerList : PubsubHost
+    {
+        [Header("Settings")]
+        [Tooltip("A list of player names")]
+        [SerializeField] private List/*<string>*/ players;
+
+        public bool CheckPlayer(string id)
+        {
+            return this.players.Contains(id);
+        }
+
+        public bool AddPlayer(string id)
+        {
+            if (this.players.Contains(id))
+                return false;
+
+            bool added = this.players.Add(id);
+
+            if (added)
+                this.BroadcastEvent(PlayerListEvent.PlayerAdded, id);
+
+            return added;
+        }
+
+        public bool RemovePlayer(string id)
+        {
+            if (!this.players.Contains(id))
+                return false;
+
+            bool removed = this.players.Remove(id);
+
+            if (removed)
+                this.BroadcastEvent(PlayerListEvent.PlayerRemoved, id);
+
+            return removed;
+        }
+    }
+}
