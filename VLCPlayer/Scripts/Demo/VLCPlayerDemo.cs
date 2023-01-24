cfed8604f54f729b3b0ac0747652e25c577dca79
DecentM <decentm@decentm.com>
2023-01-24 23:59:18 +0200

    chore(video-player): v0.0.2

---
+++ VLCPlayer/Scripts/Demo/VLCPlayerDemo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DecentM.CustomComponents.VLCPlayer
+{
+    public class VLCPlayerDemo : MonoBehaviour
+    {
+        public VLCPlayer player;
+
+        public string url = "";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (this.player == null)
+                return;
+
+            this.player.Open(this.url);
+            this.player.Play();
+        }
+    }
+}
