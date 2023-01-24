22b7f8b2146944f8544f40563824841654f93d0d
DecentM <decentm@decentm.com>
2023-01-24 23:59:18 +0200

    chore(video-player): v0.0.2

---
+++ Runtime/PerformanceLevelChangeProvider.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using DecentM.PerformanceGovernor;
+using DecentM.PerformanceGovernor.Plugins;
+
+namespace DecentM.NotificationSystem.Providers
+{
+    public class PerformanceLevelChangeProvider : PerformanceGovernorPlugin
+    {
+        public NotificationSystem notifications;
+        public Toggle toggle;
+
+        public Sprite iconHigh;
+        public Sprite iconMed;
+        public Sprite iconLow;
+
+        protected override void OnPerformanceModeChange(PerformanceGovernorMode mode, float fps)
+        {
+            switch (mode)
+            {
+                case PerformanceGovernorMode.Low:
+                    this.OnPerformanceLow();
+                    return;
+
+                case PerformanceGovernorMode.Medium:
+                    this.OnPerformanceMedium();
+                    return;
+
+                case PerformanceGovernorMode.High:
+                    this.OnPerformanceHigh();
+                    return;
+            }
+        }
+
+        private void OnPerformanceHigh()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconHigh, "Performance mode changed to High");
+        }
+
+        private void OnPerformanceMedium()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconMed, "Performance mode changed to Medium");
+        }
+
+        private void OnPerformanceLow()
+        {
+            if (!this.toggle.isOn)
+                return;
+            this.notifications.SendNotification(this.iconLow, "Performance mode changed to Low");
+        }
+    }
+}
