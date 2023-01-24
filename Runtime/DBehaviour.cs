5eca07bce8d87a185f2cba7a4e4a7df9a862305f
DecentM <decentm@decentm.com>
2023-01-24 23:59:18 +0200

    chore(video-player): v0.0.2

---
+++ Runtime/DBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+#if COMPILER_UDONSHARP
+using UdonSharp;
+#endif
+
+namespace DecentM.Shared
+{
+    public class DBehaviour
+#if COMPILER_UDONSHARP
+        : UdonSharpBehaviour
+#elif TESTS_RUNNING
+        // Defined from cli, means we can test pure classes without MonoBehaviour
+#else
+        : MonoBehaviour
+#endif
+    {
+        public void Invoke(string methodName, int delaySeconds)
+        {
+#if COMPILER_UDONSHARP
+            this.SendCustomEventDelayedSeconds(methodName, delaySeconds);
+#else
+            base.Invoke(methodName, delaySeconds);
+#endif
+        }
+    }
+}
