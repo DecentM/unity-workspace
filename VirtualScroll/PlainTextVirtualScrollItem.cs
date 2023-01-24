90cec43f0f19a43598622216cf4b481febd93ab8
DecentM <decentm@decentm.com>
2023-01-24 23:59:18 +0200

    chore(video-player): v0.0.2

---
+++ VirtualScroll/PlainTextVirtualScrollItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace DecentM.UI.Examples
+{
+    public class PlainTextVirtualScrollItem : VirtualScrollItem
+    {
+        [SerializeField] private TextMeshProUGUI slot;
+
+        private string GetText()
+        {
+            if (this.data == null) return string.Empty;
+            string text = this.data.ToString();
+            if (string.IsNullOrWhiteSpace(text)) return string.Empty;
+
+            return text;
+        }
+
+        protected override void OnDataChange()
+        {
+            this.slot.text = this.GetText();
+        }
+    }
+}
---
+++ VirtualScroll/VirtualScrollItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using DecentM.Shared;
+
+namespace DecentM.UI
+{
+    public abstract class VirtualScrollItem : DBehaviour
+    {
+        public RectTransform rectTransform;
+
+        internal int index = -1;
+        internal object data = null;
+
+        public void SetData(object data)
+        {
+            this.data = data;
+            this.OnDataChange();
+        }
+
+        protected virtual void OnDataChange() { }
+    }
+}
